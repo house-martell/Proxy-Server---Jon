f48b10a6a322c1142cf34356d98c6712169e407d
Jonathan Neumann <jonathanHneumann@gmail.com>
2019-01-05 15:09:02 -0800

    added cors functionality

--- proxy/server.js
+++ proxy/server.js
@@ -3,9 +3,11 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = 3000;
+const cors = require('cors')
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
